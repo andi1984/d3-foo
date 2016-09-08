1f63ce49bdafa48e4b24bf212194e409b4da5862
Andreas Sander <andreas.sander@hotmail.de>
2016-09-08 23:37:38 +0200

    Add ES6 -> ES5 Transpiling
    Add Babel transpiler to the project to transpile ES6 to ES5 for end usage in our products.

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,14 @@
 import npm from "rollup-plugin-node-resolve";
+import babel from 'rollup-plugin-babel';
+import babelrc from 'babelrc-rollup';
 
 export default {
   entry: "index.js",
   format: "umd",
   moduleName: "d3",
-  plugins: [npm({jsnext: true})],
+  plugins: [
+      babel(babelrc()),
+      npm({jsnext: true})
+  ],
   dest: "build/d3-foo.js"
 };
